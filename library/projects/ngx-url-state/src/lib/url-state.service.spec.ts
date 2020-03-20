1874c6a1f1192ceca797fafd6fe2079320b85a47
Matt Wilson <matt@matt-wilson.org>
2020-03-20 13:18:45 +0000

    Setup jest as per article
    See https://medium.com/angular-in-depth/migrate-your-angular-library-to-jest-faba9579591a

--- library/projects/ngx-url-state/src/lib/url-state.service.spec.ts
+++ library/projects/ngx-url-state/src/lib/url-state.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UrlStateService } from './url-state.service';
 
@@ -6,7 +7,11 @@ describe('NgxUrlStateService', () => {
   let service: UrlStateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+    });
     service = TestBed.inject(UrlStateService);
   });
 
