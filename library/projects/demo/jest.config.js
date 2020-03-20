176f6a23cda726159050bdf69be99cbb7c3af838
Matt Wilson <matt@matt-wilson.org>
2020-03-20 13:57:35 +0000

    More promising jest setup with moduleNameMapper

--- library/projects/demo/jest.config.js
+++ library/projects/demo/jest.config.js
@@ -1,4 +1,7 @@
 const baseConfig = require('../../jest.base.config');
 module.exports = {
   ...baseConfig,
+  moduleNameMapper: {
+    'ngx-url-state': '<rootDir>/../ngx-url-state/src/public-api'
+  }
 };
