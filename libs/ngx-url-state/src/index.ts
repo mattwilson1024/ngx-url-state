7e23f81fa0ae7d9370148edbb51f38977e2a1b39
Matt Wilson <matt@matt-wilson.org>
2022-05-20 13:47:17 +0100

    Trim out unused files

--- libs/ngx-url-state/src/index.ts
+++ libs/ngx-url-state/src/index.ts
@@ -3,7 +3,6 @@
  */
 
 export * from './lib/mappers';
-export * from './lib/ngx-url-state.module';
 export * from './lib/url-state.service';
 export * from './lib/url-state';
 export * from './lib/url-state.types';
